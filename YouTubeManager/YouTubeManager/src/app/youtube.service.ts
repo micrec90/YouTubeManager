dede38d4d4d09d155aeca0502ee8b3095ed21105
Michal Reclaw <micrec90@gmail.com>
2022-08-08 22:36:58 +0200

    YouTube service
    - generated YouTube service
    
    - implemented getVideosForChannel
    
    - empty YouTube API key

---
+++ YouTubeManager/YouTubeManager/src/app/youtube.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, map } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class YoutubeService {
+
+  apiKey: string = ''; // YouTube API key
+
+  constructor(public http: HttpClient) { }
+
+  getVideosForChannel(channel: string, maxResults: string): Observable<Object> {
+    let url = 'https://www.googleapis.com/youtube/v3/search?key=' + this.apiKey + '&channelId=' + channel + '&order=date&part=snippet &type=video,id&maxResults=' + maxResults
+    return this.http.get(url).pipe(map((res) => { return res;}))
+
+  }
+}
